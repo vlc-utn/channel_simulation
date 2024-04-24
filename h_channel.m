bee9961395649fe4fdafdb769df00c519344f627
ncotti <ncotti@frba.utn.edu.ar>
2024-04-24 12:49:01 -0300

    Optimize h_channel with vectorization

--- h_channel.m
+++ h_channel.m
@@ -35,30 +35,25 @@ function [h] = h_channel(r_s, n_s, m, h_s, r_r, n_r, A, FOV, t_vector)
 
     for j=1:1:height(r_s)
         % Pre-allocate vectors
-        distance = zeros(height(r_r), 1);
-        cos_emitter = zeros(size(distance));
-        cos_receiver = zeros(size(distance));
         h_aux = zeros(height(r_r), length(t_vector));
     
         % Vector operations
-        for i=1:1:length(r_r)
-            distance(i) = norm(r_s(j,:) - r_r(i,:));
-            cos_emitter(i) = dot(n_s(j,:), (r_r(i,:) - r_s(j,:)) ./ distance(i));
-            cos_receiver(i) = dot(n_r(i,:), (r_s(j,:) - r_r(i,:)) ./ distance(i));
-        end
+        distance = vecnorm(r_s(j,:) - r_r, 2, 2);
+        cos_emitter = dot(repmat(n_s(j,:), height(r_r), 1), (r_r - r_s(j,:)) ./ distance, 2);
+        cos_receiver = dot(n_r, (r_s(j,:) - r_r) ./ distance, 2);
         cos_emitter(cos_emitter < 0) = 0;
 
+        channel_att = ((m+1) / (2*pi)) .* cos_emitter.^m .* A .* cos_receiver ...
+            .* rect(acosd(cos_receiver) / FOV) ./ (distance.^2);
+
+        [~, index] = min(abs(distance/physconst("LightSpeed") - t_vector), [], 2);
+
         for i=1:1:length(r_r)
-            [~, index] = min(abs(distance(i)/physconst("LightSpeed") - t_vector));
-            h_aux(i,index) = ((m+1) / (2*pi)) .* cos_emitter(i).^m .* A .* cos_receiver(i) ...
-            .* rect(acosd(cos_receiver(i)) / FOV) ./ (distance(i).^2);
+            h_aux(i,index(i)) = channel_att(i);
         end
 
-        h_conv = zeros(height(r_r), width(h_aux) + width(h_s) -1);
-        for i=1:1:length(r_r)
-            h_conv(i,:) = conv(h_s(j,:), h_aux(i,:), "full");
-            h_aux(i,:) = h_conv(i, 1:length(t_vector));
-        end
+        h_conv = conv2(h_aux, h_s(j,:), "full");
+        h_aux = h_conv(:, 1:length(t_vector));
         h = h + h_aux;
     end
 end
