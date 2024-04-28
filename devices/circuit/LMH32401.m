0c9741f3b4c0d357dbad49dc4e8161f88b9d218e
ncotti <ncotti@frba.utn.edu.ar>
2024-04-28 14:19:01 -0300

    Correct input capacitance

--- devices/circuit/LMH32401.m
+++ devices/circuit/LMH32401.m
@@ -9,7 +9,7 @@ dia_df = (3e-12)^2;             % [A^2/Hz] Shunt noise current at the input from
 dea_df = (17.8e-9)^2;           % [V^2/Hz] Series noise voltage at the input from the amplifier.
 
 Rin = 350;                      % [Ohm] Input impedance
-Ca = 0.1e-9;                    % [Farad] Input capacitance of the circuit (not the photodiode)
+Ca = 0;                         % [Farad] Input capacitance of the circuit (not the photodiode)
 
 B = 50e6;                       % [Hz] Bandwidth
 I2 = 0.562;                     % Noise-bandwidth factor.
